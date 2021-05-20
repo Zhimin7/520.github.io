adbf66ae292e92b607ee812fd1dd015b8121f142
Zhimin7 <guzhimin7@gmail.com>
2021-05-20 21:30:16 +0800

    520

--- js/talk.js
+++ js/talk.js
@@ -3,7 +3,7 @@ new TypeIt("#tqyxhj", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("TQY && XHJ")
+    .type("GZM && KMX")
     .pause(2000)
     .delete(null, {
         delay: 500
